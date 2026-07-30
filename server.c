d2d6039d0a31fa19c18c30563639fd8cb57c82fe
Alexandre <94722271+Reuillon@users.noreply.github.com>
2026-07-30 12:26:01 -0400

    Added file caching

--- server.c
+++ server.c
@@ -8,12 +8,32 @@
 #include <unistd.h>
 #include <string.h>
 
-//LOADS THE DATA FROM GET REQUEST ON THE WEBSERVER
-void LoadGetRequest(FILE* f, SSL* ssl, char* metadata);
 
+typedef struct 
+{
+	char* data;
+	char fileName[256];
+	size_t fileSize;
+}CachedFile;
+
+CachedFile* LoadCacheList();
+
+int cachedFileSize = 0;
+
+//LOADS THE DATA FROM GET REQUEST ON THE WEBSERVER
+void LoadGetRequest(char* filename, SSL* ssl, char* metadata);
+
+CachedFile InitCachedFile(char* fileName, int isTextFile);
+
+int CheckFileExtenstion(char* FileName);
+
+char* metadata;
+
+CachedFile* cachedFiles;
 
 int main() 
 {
+	cachedFiles = LoadCacheList();
 	
 	//CREATES SOCKET
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -83,11 +103,14 @@ int main()
 	timeout.tv_sec = 5;
 	timeout.tv_usec = 0;
 
+	metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+
 	//MAIN LOOP FOR SENDING AND RECEIVING
 	while (1)
 	{
+
 		//CHECKS HEADER FOR CONTENT TYPE IN THIS CASE FOR IMAGES
-		int isImage = 0;
+		int isTextFile = 1;
 		printf("awaiting connection....\n");
 		
 		int clientfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientLen);
@@ -139,7 +162,7 @@ int main()
 			}
 			buffer[sslRead] = '\0';
 			
-			char* metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+			
 			
 			char method[16];
 			char filename[256];
@@ -148,87 +171,19 @@ int main()
 			//CHECK IF REQUEST IS A VALID GET REQUEST
 			if (sscanf(buffer, "%15s /%255s %15s", method, filename, version) == 3 && strcmp(method, "GET") == 0)
 			{
-				if (strstr(filename, ".css"))
-				{
-					isImage = 0;
-					char* newMetaData = "HTTP/1.1 200 OK\r\nContent-Type: text/css\r\n\r\n";
-					metadata = newMetaData;
-				}
-				else if (strstr(filename, ".jpg") || strstr(filename, ".jpeg"))
-				{
-					isImage = 1;
-					char* newMetaData = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n";
-					metadata = newMetaData;
-				}
-				else if (strstr(filename, ".png"))
-				{
-					isImage = 1;
-					char* newMetaData = "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n\r\n";
-					metadata = newMetaData;
-				}
-				else if (strchr(filename, '%') != NULL || strchr(filename, '/') != NULL || strchr(filename, '\\')  != NULL || strstr(filename, "..")  != NULL || strlen(filename) > 100)
-				{
-					printf("BAD URL\n\n");
-					isImage = -1;
-					char* newMetaData = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-					snprintf(filename, sizeof(filename), "index.html");
-					metadata = newMetaData;
-				}
-				else
-				{
-					isImage = 0;
-					char* newMetaData = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-					metadata = newMetaData;
-				}
+				isTextFile = CheckFileExtenstion(filename);
 				
-				//IF FILE TYPE IS PLAINTEXT
-				if (isImage == 0)
-				{				
-					FILE* f = fopen(filename, "r");
-					if (f)
-					{
-						LoadGetRequest(f, ssl, metadata);
-						fclose(f);
-					}
-					else
-					{
-						FILE* defaultPage = fopen("index.html", "r");
-						if (defaultPage)
-						{
-							LoadGetRequest(defaultPage, ssl, metadata);
-							fclose(defaultPage);
-						}
-					}
-				}
-				//IF FILE TYPE IS AN IMAGE
-				else if (isImage == 1)
+				
+
+				//IF FILE TYPE IS A VALID FILETYPE
+				if (isTextFile != -1)
 				{
-					FILE* f = fopen(filename, "rb");
-					if (f)
-					{
-						LoadGetRequest(f, ssl, metadata);
-						fclose(f);
-					}	
-					else
-					{
-						FILE* defaultPage = fopen("index.html", "r");
-						if (defaultPage)
-						{
-							char* defaultMetadata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-							LoadGetRequest(defaultPage, ssl, defaultMetadata);
-							fclose(defaultPage);
-						}
-					}	
+					LoadGetRequest(filename, ssl, metadata);	
 				}
 				//IF UNKNOWN/UNAUTHORIZED FILE EXTENSION OR FILE NAME DEFAULT TO HOMEPAGE
 				else
 				{
-					FILE* defaultPage = fopen("index.html", "r");
-					if (defaultPage)
-					{
-						LoadGetRequest(defaultPage, ssl, metadata);
-						fclose(defaultPage);
-					}
+					LoadGetRequest("index.html", ssl, metadata);	
 				}
 			}
 			//IF REQUEST IS MALFORMED OR NOT A GET REQUEST
@@ -246,22 +201,205 @@ int main()
 
     SSL_CTX_free(ctx);
     close(sockfd);
+	if (cachedFileSize != 0)
+	{
+		for (int i = 0; i < cachedFileSize; i++)
+		{
+			free(cachedFiles[i].data);
+		}
+		free(cachedFiles);
+	}
+}
+
+CachedFile* LoadCacheList()
+{
+	int File_Amount = 0;
+	int readChar;
+	int lastChar = '\0';
+	FILE* List_Cached_Files = fopen("CachedFilesList.txt", "r");
+	while((readChar = fgetc(List_Cached_Files)) != EOF)
+	{
+		if (readChar == '\n')
+		{
+			++File_Amount;
+		}
+		lastChar = readChar;
+	}
+	if (lastChar != '\n' && lastChar != '\0')
+	{
+		++File_Amount;
+	} 
+	
+	cachedFileSize = File_Amount;
+	CachedFile* cachedFiles = malloc(sizeof(CachedFile) * File_Amount);
+	
+	rewind(List_Cached_Files);
+	char line[256];
+	int currentFileIndex = 0;
+	while (fgets(line, sizeof(line), List_Cached_Files))
+	{
+		line[strcspn(line, "\r\n")] = '\0';
+		cachedFiles[currentFileIndex] = InitCachedFile(line, CheckFileExtenstion(line));
+		++currentFileIndex;
+	}
+	fclose(List_Cached_Files);
+	return cachedFiles;
+}
+
+//LOADS A FILE DIRECTLY INTO MEMORY TO PREVENT MULTIPLE FILE READS
+CachedFile InitCachedFile(char* fileName, int isTextFile)
+{
+	CachedFile newFile;
+	
+	if (isTextFile == 1)
+	{
+		FILE* loadedFile = fopen(fileName, "rb");;
+		if (loadedFile)
+		{
+			fseek(loadedFile, 0, SEEK_END);
+			size_t fileSize = ftell(loadedFile);
+			rewind(loadedFile);
+			newFile.data = malloc(fileSize);
+			newFile.fileSize = fileSize;
+			strcpy(newFile.fileName, fileName);
+			fread(newFile.data, 1, fileSize + 1, loadedFile);
+			newFile.data[fileSize] = '\0';
+			fclose(loadedFile);
+		}
+		else
+		{
+			printf("ERROR LOADING FILE %s INTO MEMORY! ABORTING SERVER\n", fileName);
+			exit(-1);
+		}
+	}
+	else if (isTextFile != 1)
+	{
+		FILE* loadedFile = fopen(fileName, "rb");;
+		if (loadedFile)
+		{
+			fseek(loadedFile, 0, SEEK_END);
+			size_t fileSize = ftell(loadedFile);
+			rewind(loadedFile);
+			newFile.data = malloc(fileSize);
+			newFile.fileSize = fileSize;
+			strcpy(newFile.fileName, fileName);
+
+			fread(newFile.data, 1, fileSize, loadedFile);
+			fclose(loadedFile);
+		}
+		else
+		{
+			printf("ERROR LOADING FILE %s INTO MEMORY! ABORTING SERVER", fileName);
+			exit(-1);
+		}
+	}
+
+	return newFile;
 }
 
 //LOADS THE DATA FROM GET REQUEST ON THE WEBSERVER
-void LoadGetRequest(FILE* f, SSL* ssl, char* metadata)
+void LoadGetRequest(char* filename, SSL* ssl, char* metadata)
 {
-	char buffer[65536];
-	size_t bytesRead;
+	//IF THERE IS NO ESTABLISHED SSL CONNECTION DO NOTHING
 	if (SSL_write(ssl, metadata, strlen(metadata)) <= 0)
 	{
 		return;
 	}
-	while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0)
+
+	int CacheExists = 0;	
+	//CHECKS IF FILE REQUEST EXISTS ON DISK
+	for(int i = 0; i < cachedFileSize; i++)
 	{
-		if (SSL_write(ssl, buffer, bytesRead) <= 0)
+		if (strncmp(cachedFiles[i].fileName, filename, strlen(filename)) == 0)
 		{
+			printf("WritingFromCache!\n");
+			CacheExists = 1;
+			SSL_write(ssl, cachedFiles[i].data, cachedFiles[i].fileSize);
 			return;
 		}
 	}
+	//IF NO CACHE FILE EXIST THEN SERVER ATTEMPTS TO LOAD FROM DISK
+	if (CacheExists != 1)
+	{
+		printf("WritingFromDisk!\n");
+		FILE* f = fopen(filename, "rb");
+		char buffer[65536];
+		size_t bytesRead; 
+			
+		if (f)
+		{			
+			while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0)
+			{
+				if (SSL_write(ssl, buffer, bytesRead) <= 0)
+				{
+					return;
+				}
+			}
+			fclose(f);
+		}
+		else
+		{
+			f = fopen("index.html", "rb");
+			if (f)
+			{
+				while ((bytesRead = fread(buffer, 1, sizeof(buffer), f)) > 0)
+				{
+					if (SSL_write(ssl, buffer, bytesRead) <= 0)
+					{
+						return;
+					}
+				}
+				fclose(f);
+			}
+			else
+			{
+				//THIS REALLY SHOULDNT BE POSSIBLE BUT IT IS A FAILSAFE
+				printf("ERROR NO FILE FOUND!\n");
+			}
+			
+		}
+	}
+}
+
+//CHECKS EXTENSION OF FILENAME AND SETS METADATA TO CORRECT VALUES
+int CheckFileExtenstion(char* fileName)
+{
+	if (strstr(fileName, ".html"))
+	{
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+		return 1;
+	}
+	else if (strstr(fileName, ".css"))
+	{
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/css\r\n\r\n";
+		return 1;
+	}
+	else if (strstr(fileName, ".js"))
+	{
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/javascript\r\n\r\n";
+		return 1;
+	}
+	else if (strstr(fileName, ".jpg") || strstr(fileName, ".jpeg"))
+	{
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n";
+		return 0;
+	}
+	else if (strstr(fileName, ".png"))
+	{
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n\r\n";
+		return 0;
+	}
+	else if (strchr(fileName, '%') != NULL || strchr(fileName, '/') != NULL || strchr(fileName, '\\')  != NULL || strstr(fileName, "..")  != NULL || strlen(fileName) > 100)
+	{
+		printf("BAD URL\n\n");
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+		return -1;
+	}
+	else
+	{
+		metadata = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+		return -1;
+	}
+
+	return 1;
 }
