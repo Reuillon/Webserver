3596b8df57766934890fe46c4d5adedfb2048962
Alexandre <94722271+Reuillon@users.noreply.github.com>
2026-08-15 21:39:25 -0400

    Delete clock.js

--- clock.js
+++
@@ -1,8 +0,0 @@
-function updateClock() 
-{
-  const now = new Date();
-  const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
-  document.getElementById('clock').textContent = timeString;
-}
-updateClock();
-setInterval(updateClock, 1000);
